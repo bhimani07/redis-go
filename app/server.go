51ef5e51af8ba6f9f3ae3b401d6695e47d4779d5
Kishan Bhimani <kishanbhimani@Kishans-MacBook-Pro-2.local>
2022-11-29 14:18:29 -0500

    tcp server binding added

--- app/server.go
+++ app/server.go
@@ -2,25 +2,28 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
+	"net"
+	"os"
+	"log"
 )
+
+const NETWORK_TYPE = "tcp"
+const HOST = "localhost"
+const PORT = "6379"
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
+	listener, err := net.Listen(NETWORK_TYPE, HOST+":"+PORT)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
 
-	// Uncomment this block to pass the first stage
-	//
-	// l, err := net.Listen("tcp", "0.0.0.0:6379")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 6379")
-	// 	os.Exit(1)
-	// }
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
+	_, err1 := listener.Accept()
+	if err1 != nil {
+		log.Fatal(err1)
+		os.Exit(1)
+	}
+
 }
